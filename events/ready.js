5d7ffd71d8efba082951e43f158c618fea5450f2
Ahmet İnci <prtkgmr@gmail.com>
2023-06-13 00:34:01 +0300

    console log every guild that the bot is in

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}` client.guilds.cache.map(guild => guild.id).join(", \n"));
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(client.guilds.cache.map(guild => guild.id).join(", \n"));
 	},
 };
